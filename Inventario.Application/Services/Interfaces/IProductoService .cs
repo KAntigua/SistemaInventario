70c3e9158528ec6cc3cf9a60afa8c9e25a7d20b1
KAntigua <karenantigua31@gmail.com>
2026-05-17 17:39:50 -0400

    Servicio interface de producto

---
+++ Inventario.Application/Services/Interfaces/IProductoService .cs
@@ -0,0 +1,17 @@
+﻿using Inventario.Application.DTOs.Producto;
+
+namespace Inventario.Application.Services.Interfaces
+{
+    public interface IProductoService
+    {
+
+        Task<IEnumerable<ProductoDTO>> GetAllAsync();
+        Task<ProductoDTO> GetByIdAsync(int id);
+        Task<ProductoDTO> CreateAsync(CrearProductoDTO dto);
+        Task<ProductoDTO> UpdateAsync(int id,ActualizarProductoDTO dto);
+        Task DeleteAsync(int id);
+
+        Task<IEnumerable<ProductoDTO>> GetProductosBajoStockAsync();
+
+    }
+}
